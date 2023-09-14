6978cbcad25391c7afda1bfc79701df2b41bc791
helt1nh0 <131688858+helt1nh0@users.noreply.github.com>
2023-09-14 11:17:58 -0300

    h

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 function manu () {
     document.querySelector('#som_tecla_toim').play();
 }
+const lista = document.querySelectorAll('.tecla');
 
 document.querySelector('.tecla_pom').onclick=manu
 
