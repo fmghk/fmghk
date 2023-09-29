e51500303c07091d13520a7622e1e4fbfc4fb57e
helt1nh0 <131688858+helt1nh0@users.noreply.github.com>
2023-09-29 12:06:02 -0300

    h

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ while(Contador < lista.length){
     //console.log (Contador);
     tecla.onkeydown = function (evento){
         tecla.classList.add('ativa');
-        console.log(evento.cod) } 
+        console.log(evento.cod === 'Space') } 
 
     tecla.onkeyup = function () {
         tecla.classList.remove('ativa');
