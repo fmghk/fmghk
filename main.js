f4ede65c0bcc0136b1ef9c557643b2732e7b0386
helt1nh0 <131688858+helt1nh0@users.noreply.github.com>
2023-09-29 11:38:20 -0300

    x

--- main.js
+++ main.js
@@ -6,19 +6,18 @@ const lista = document.querySelectorAll('.tecla');
 let Contador = 0;
 
 const classe= lista[Contador].classList[1];
-console.log(classe);
-
 
 while(Contador < lista.length){
-
-    lista[Contador].onclick= function(){ 
-        manu(idAudio);
-    }
-    
     const classe= lista[Contador].classList[1];
     console.log(classe);
     const idAudio = `#som_${classe}`;
     console.log(idAudio);
+    
+    lista[Contador].onclick= function(){ 
+        manu(idAudio);
+    }
+    
+    
     Contador = Contador + 1;
     console.log (Contador);
     
